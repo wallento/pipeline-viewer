638f91b54e5d40d7e0c33e2d774779bfb39294fc
Stefan Wallentowitz <stefan@wallentowitz.de>
2019-05-28 10:38:27 +0200

    Make pipeline trace regex local and add BHT/BP

--- pipelineviewer/main.py
+++ pipelineviewer/main.py
@@ -6,15 +6,6 @@ from attrdict import AttrDict
 import colorama
 import argparse
 
-# IF log is "<cycle> IF <id> <mode> <addr>"
-trace_if = re.compile(r"^\s*(\d+) IF \s*(\d+) (\w) ([0-9A-Fa-f]+)")
-# DE log is "<cycle> DE <id> <addr> <insn>"
-trace_de = re.compile(r"^\s*(\d+) DE \s*(\d+) ([0-9A-Fa-f]+) (.*)")
-# IS log is "<cycle> IS <id>"
-trace_is = re.compile(r"^\s*(\d+) IS \s*(\d+)")
-# C log is "<cycle> C <id>"
-trace_c = re.compile(r"^\s*(\d+) C \s*(\d+)")
-
 display = {"IF": AttrDict(char="f", fore=colorama.Fore.WHITE, back=colorama.Back.BLUE),
            "DE": AttrDict(char="d", fore=colorama.Fore.WHITE, back=colorama.Back.YELLOW),
            "IS": AttrDict(char="i", fore=colorama.Fore.WHITE, back=colorama.Back.RED),
@@ -28,16 +19,34 @@ class Pipeline(object):
 
 class PipelineAriane(Pipeline):
     stages = ["IF", "DE", "IS", "C"]
+
+    # IF log is "<cycle> IF <id> <mode> <addr>"
+    trace_if = re.compile(r"^\s*(\d+) IF \s*(\d+) (\w) ([0-9A-Fa-f]+)")
+    # DE log is "<cycle> DE <id> <addr> <insn>"
+    trace_de = re.compile(r"^\s*(\d+) DE \s*(\d+) ([0-9A-Fa-f]+) (.*)")
+    # IS log is "<cycle> IS <id>"
+    trace_is = re.compile(r"^\s*(\d+) IS \s*(\d+)")
+    # C log is "<cycle> C <id>"
+    trace_c = re.compile(r"^\s*(\d+) C \s*(\d+)")
+
+    # BHT log is "<cycle> BHT <id> <pc> <index> <valid> <taken>: <old>-><new>"
+    trace_bht = re.compile(r"^\s*(\d+) BHT\s+(\d+) ([0-9A-Fa-f]+)\s+(\d+) \[(\d)\] (\d): (\d+)->(\d+)")
+    # BP STATIC log is  "<cycle> BP STATIC <id> <pc> <index> <direction>"
+    trace_bp_static = re.compile(r"^\s*(\d+) BP STATIC \s*(\d+) ([0-9A-Fa-f]+)\s+(\d+) (\d)")
+    # BP STATIC log is  "<cycle> BP DYNAMIC <id> <pc> <index> <direction>"
+    trace_bp_dynamic = re.compile(r"^\s*(\d+) BP DYNAMIC \s*(\d+) ([0-9A-Fa-f]+)\s+(\d+) (\d+)")
+
     def __init__(self, file):
         log = {}
 
         for line in file:
-            m = trace_if.match(line)
+            m = self.trace_if.match(line)
             if m:
                 id = int(m.group(2))
-                log[id] = AttrDict({"pc": int(m.group(4),16), "insn": None, "mode": m.group(3), "IF": int(m.group(1)), "DE": None, "IS": None, "C": None})
+                log[id] = AttrDict({"pc": int(m.group(4),16), "insn": None, "mode": m.group(3), "IF": int(m.group(1)),
+                                    "DE": None, "IS": None, "C": None, "BHT": None, "BP": None})
                 continue
-            m = trace_de.match(line)
+            m = self.trace_de.match(line)
             if m:
                 id = int(m.group(2))
                 pc = int(m.group(3),16)
@@ -46,16 +55,33 @@ class PipelineAriane(Pipeline):
                 log[id].DE = int(m.group(1))
                 log[id].insn = m.group(4)
                 continue
-            m = trace_is.match(line)
+            m = self.trace_is.match(line)
             if m:
                 id = int(m.group(2))
                 log[id].IS = int(m.group(1))
                 continue
-            m = trace_c.match(line)
+            m = self.trace_c.match(line)
             if m:
                 id = int(m.group(2))
                 log[id].C = int(m.group(1))
                 continue
+            m = self.trace_bht.match(line)
+            if m:
+                id = int(m.group(2))
+                log[id].BHT = AttrDict(index=int(m.group(4)), taken=int(m.group(6)), oldcounter=int(m.group(7), 2),
+                                       newcounter=int(m.group(8), 2))
+                continue
+            m = self.trace_bp_static.match(line)
+            if m:
+                id = int(m.group(2))
+                log[id].BP = AttrDict(type="static", index=int(m.group(4)), taken=int(m.group(5)))
+                continue
+            m = self.trace_bp_dynamic.match(line)
+            if m:
+                id = int(m.group(2))
+                log[id].BP = AttrDict(type="dynamic", index=int(m.group(4)), taken=(int(m.group(5), 2) >= 2))
+                continue
+
         self.log = log
 
 
@@ -104,6 +130,18 @@ def render(pipeline, args):
 
         if not args.no_insn and i.insn:
             line += " " + i.insn
+
+        if args.bp:
+            if "BP" in i and i.BP:
+                if i.BP.taken:
+                    line += ", BP taken @{} ({})".format(i.BP.index, i.BP.type)
+                else:
+                    line += ", BP not taken @{} ({})".format(i.BP.index, i.BP.type)
+            if "BHT" in i and i.BHT:
+                if i.BHT.taken:
+                    line += ", BHT @{} taken ({:02b}->{:02b})".format(i.BHT.index, i.BHT.oldcounter, i.BHT.newcounter)
+                else:
+                    line += ", BHT @{} not taken ({:02b}->{:02b})".format(i.BHT.index, i.BHT.oldcounter, i.BHT.newcounter)
 
         args.outfile.write(line+"\n")
     colorama.deinit()
